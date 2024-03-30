04b6ad33c976c01966de23187e03900bb7fb5da1
Allysonfreitas210695 <alisonfr83@gmail.com>
2024-03-29 22:23:06 -0300

    ajuste no model Address

--- apps/profiles/serializers.py
+++ apps/profiles/serializers.py
@@ -1,6 +1,7 @@
 from .models import Profile
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from django.db import transaction
 
 class ProfileSerializer(serializers.ModelSerializer):
 
@@ -14,23 +15,28 @@ class ProfileSerializer(serializers.ModelSerializer):
             if get_user_model().objects.filter(email=validated_data['email']).exists():
                 raise serializers.ValidationError("Erro! Já existe um usuário com o mesmo e-mail.")
             
-            # antes de inserir verificar se exite ou nao , em caso de nao criar se sim estoura um erro
-            user = get_user_model().objects.create_user(
-                username=validated_data['email'],
-                email=validated_data['email'],
-                password=validated_data['password']
-            )
+            with transaction.atomic():
+                # antes de inserir verificar se exite ou nao , em caso de nao criar se sim estoura um erro
+                user = get_user_model().objects.create_user(
+                    username=validated_data['email'],
+                    email=validated_data['email'],
+                    password=validated_data['password']
+                )
 
-            user.is_staff = True
-            user.is_superuser = True
-            user.save()
+                user.is_staff = True
+                user.is_superuser = True
+                user.save()
 
-            profile_serializer = self.Meta.model(user=user, **validated_data)
-            profile_serializer.save()
+                # Removendo o campo 'id' dos dados validados
+                validated_data.pop('id', None)
+
+                profile_serializer = self.Meta.model(**validated_data)
+                profile_serializer.save()
 
             return validated_data
         except Exception as e:
+            # Se ocorrer um erro, desfazer a criação do usuário
+            user.delete()
             raise serializers.ValidationError(e)
-
 
 
