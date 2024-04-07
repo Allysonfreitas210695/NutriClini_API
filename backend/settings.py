1d544e11b7da9882e5aa82b06946c4552310f9ea
Allysonfreitas210695 <alisonfr83@gmail.com>
2024-04-07 13:25:41 -0300

    ajuste na api

--- backend/settings.py
+++ backend/settings.py
@@ -11,7 +11,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY', default='mysecretkey')
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = [
     '*'
